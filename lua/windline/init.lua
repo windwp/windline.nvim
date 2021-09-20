ac07922420cece7c75e73205387ffd0f44473d50
zztrieuzz <longtrieu1912@gmail.com>
2021-09-20 15:53:44 +0700

    refactor: move benchmark code to new file

--- lua/windline/init.lua
+++ lua/windline/init.lua
@@ -128,7 +128,7 @@ end
 
 local setup_hightlight = function(colors)
     assert(M.default_line ~= nil, 'you need define default statusline')
-    assert(M.default_line.active ~= nil, 'default need list active componet')
+    assert(M.default_line.active ~= nil, 'default need list active component')
     assert(M.default_line.inactive ~= nil, 'default need list inactive component')
 
     if M.anim_pause then M.anim_pause() end
@@ -198,7 +198,7 @@ M.setup = function(opts)
         augroup END]],
         false
     )
-    api.nvim_exec("command! -nargs=* WindLineBenchmark lua require('windline').benchmark()", false)
+    api.nvim_exec("command! -nargs=* WindLineBenchmark lua require('windline.benchmark').benchmark()", false)
 end
 
 
@@ -268,50 +268,6 @@ M.remove_component = function(opt)
             break
         end
     end
-end
-
-
---- a benchmark current statusline. it need plenary.nvim
-M.benchmark = function()
-    local num = 1e4
-    local bench = require('plenary.profile').benchmark
-    local statusline = ''
-    local time = bench(num, function()
-        vim.g.statusline_winid = api.nvim_get_current_win()
-        statusline = WindLine.show(api.nvim_get_current_buf(), vim.g.statusline_winid)
-    end)
-    local popup = require('plenary.popup')
-    local result = {}
-    table.insert(result,'Status:')
-    table.insert(result, statusline)
-    table.insert(result, 'Time:')
-    table.insert(result, string.format('render %s time : *%s*', num, time))
-    table.insert(result, 'Comp:')
-    local line = M.get_statusline_ft(vim.bo.filetype) or M.default_line
-    local bufnr = api.nvim_get_current_buf()
-    local winid = api.nvim_get_current_win()
-    local width = api.nvim_win_get_width(0)
-    table.insert(result, string.format('%s %12s %12s %s %s', ' ', 'time', 'name', 'num   ', 'text'))
-    for index, comp in pairs(line.active) do
-        local item = ''
-        time = bench(num, function()
-            M.state.comp = {}
-            Comp.reset()
-            item = comp:render(bufnr, winid, width)
-        end)
-        table.insert(result, string.format('%02d *%10s* %12s %s - %s', index, time, comp.name or '   ', num, item))
-    end
-    local vim_width = math.floor(vim.o.columns / 1.5)
-    local col = math.floor((vim.o.columns - vim_width) / 2)
-    popup.create(result, {
-        border = {},
-        minheight = 30,
-        maxwidth=vim_width,
-        col = col,
-        line = 10,
-        width = vim_width,
-    })
-    vim.bo.filetype = 'help'
 end
 
 return M
