ec855033e500758e5176d46888320133899462f9
zztrieuzz <trieu19@gmail.com>
2021-10-21 14:10:52 +0700

    fix light theme better color

--- lua/windline/themes/init.lua
+++ lua/windline/themes/init.lua
@@ -52,6 +52,15 @@ M.get_hl_color = function(hl)
     return fg, bg
 end
 
+local function is_dark(color_value)
+    local bg_numeric_value = 0;
+    for s in color_value:gmatch("[a-fA-F0-9][a-fA-F0-9]") do
+        bg_numeric_value = bg_numeric_value + tonumber("0x"..s);
+    end
+    local is_dark_bg = (bg_numeric_value < 383)
+    return is_dark_bg
+end
+
 M.generate_theme = function()
     local default = get_default_theme()
     local colors = {
@@ -72,6 +81,16 @@ M.generate_theme = function()
         cyan_light             = vim.g.terminal_color_14,
         white_light            = vim.g.terminal_color_15,
     }
+
+    if is_dark(colors.black) and vim.o.background == 'light'then
+        -- swap some color to match light theme
+        local tmp = colors.black
+        colors.black = colors.white
+        colors.white = tmp
+        tmp = colors.black_light
+        colors.black_light = colors.white_light
+        colors.white_light =tmp
+    end
 
     local fgNormal, bgNormal = M.get_hl_color('Normal')
     colors.NormalFg = fgNormal or colors.white_light
