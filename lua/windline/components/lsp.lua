0ef42d555fbbc6b5ad983aa3ececd471226888f6
Alexander Popov <alex.popov.tech@gmail.com>
2021-11-29 19:48:43 +0200

    fix typo

--- lua/windline/components/lsp.lua
+++ lua/windline/components/lsp.lua
@@ -33,7 +33,7 @@ local lsp_client_names = function(bufnr, opt)
     opt = opt or {}
     local clients = {}
     local icon = opt.icon or ' '
-    local sep = opt.seprator or '|'
+    local sep = opt.separator or '|'
 
     for _, client in pairs(vim.lsp.buf_get_clients(bufnr or 0)) do
         clients[#clients + 1] = client.name
