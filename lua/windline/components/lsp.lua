c3221047d98b2c3dd3b3b40188440f8cf5e27e58
zztrieuzz <zztrieuzz@users.noreply.github.com>
2024-05-23 16:52:27 +0700

    fix #73

--- lua/windline/components/lsp.lua
+++ lua/windline/components/lsp.lua
@@ -19,14 +19,18 @@ local get_diagnostics_count = function(bufnr)
 end
 
 local get_lsp_client = nil
-if vim.fn.has('nvim-0.8') > 0 then
+if vim.fn.has('nvim-0.10') > 0 then
+    get_lsp_client = function(bufnr)
+        return vim.lsp.get_clients({
+            bufnr = bufnr or vim.api.nvim_get_current_buf()
+        })
+    end
+else
     get_lsp_client = function(bufnr)
         return vim.lsp.get_active_clients({
             bufnr = bufnr or vim.api.nvim_get_current_buf()
         })
     end
-else
-    get_lsp_client = vim.lsp.buf_get_clients
 end
 
 local function is_lsp(bufnr, opt)
