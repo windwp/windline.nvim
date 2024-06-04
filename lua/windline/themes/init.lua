6d269b042f5d3ffd62027935dd9fa667a03f8c27
zztrieuzz <zztrieuzz@users.noreply.github.com>
2024-06-05 06:32:05 +0700

    update deprecated api

--- lua/windline/themes/init.lua
+++ lua/windline/themes/init.lua
@@ -43,10 +43,10 @@ M.clear_cache = function()
 end
 
 M.get_hl_color = function(group_name)
-    local ok, hl = pcall(vim.api.nvim_get_hl_by_name, group_name, true)
+    local ok, hl = pcall(vim.api.nvim_get_hl, 0, { name = group_name })
     if not ok then return nil, nil end
-    local fg = hl.foreground and '#' .. bit.tohex(hl.foreground, 6)
-    local bg = hl.background and '#' .. bit.tohex(hl.background, 6)
+    local fg = hl.fg and '#' .. bit.tohex(hl.fg, 6)
+    local bg = hl.bg and '#' .. bit.tohex(hl.bg, 6)
     return fg, bg
 end
 
